2aeea43c566784fb2ce5608930e99d5af60fd60a
ag993 <albingeorge@ulsecure-belknap-193136.wireless.louisville.edu>
2013-12-05 15:29:37 -0500

    Intial Elcipse Project Creation

---
+++ MainProject/gen/com/frenchtoastmafia/pokketomonsuto/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.frenchtoastmafia.pokketomonsuto;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
