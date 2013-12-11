33d7041bafa801d7ed9d2fc287d83bdd1fa19c64
Joseph Blackett <joseph.blackett@gmail.com>
2013-12-11 17:43:34 -0500

    initial commit for pokemon class

---
+++ MainProject/src/Pokemon.java
@@ -0,0 +1,17 @@
+import com.frenchtoastmafia.pokketomonsuto.R;
+import com.frenchtoastmafia.pokketomonsuto.R.string;
+
+
+public class Pokemon {
+	public string mPokemonName;
+	public int mPokemonHP;
+	public int mPokemonAttack;
+	public int mPokemonDefense;
+	public int mPokemonSpecialAttack;
+	public int mPokemonSpecialDefense;
+	public int mPokemonSpeed;
+	
+	public Pokemon(string name, int hP, int attack, int defense, int specialAttack, int specialDefense, int speed){
+		
+	};
+}
