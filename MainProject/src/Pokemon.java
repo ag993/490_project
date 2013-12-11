4e972078fa9365b097851cc16b7fee861eaf7b90
Joseph Blackett <joseph.blackett@gmail.com>
2013-12-11 17:53:10 -0500

    class getters and setters

--- MainProject/src/Pokemon.java
+++ MainProject/src/Pokemon.java
@@ -1,4 +1,3 @@
-import com.frenchtoastmafia.pokketomonsuto.R;
 import com.frenchtoastmafia.pokketomonsuto.R.string;
 
 
@@ -12,6 +11,76 @@ public class Pokemon {
 	public int mPokemonSpeed;
 	
 	public Pokemon(string name, int hP, int attack, int defense, int specialAttack, int specialDefense, int speed){
+		name = mPokemonName;
 		
+		hP = mPokemonHP;
+		
+		attack = mPokemonAttack;
+		
+		defense = mPokemonDefense;
+		
+		specialAttack = mPokemonSpecialAttack;
+		
+		specialDefense = mPokemonSpecialDefense;
+		
+		speed = mPokemonSpeed;
+	}
+
+	public string getPokemonName() {
+		return mPokemonName;
+	}
+
+	public void setPokemonName(string pokemonName) {
+		mPokemonName = pokemonName;
+	}
+
+	public int getPokemonHP() {
+		return mPokemonHP;
+	}
+
+	public void setPokemonHP(int pokemonHP) {
+		mPokemonHP = pokemonHP;
+	}
+
+	public int getPokemonAttack() {
+		return mPokemonAttack;
+	}
+
+	public void setPokemonAttack(int pokemonAttack) {
+		mPokemonAttack = pokemonAttack;
+	}
+
+	public int getPokemonDefense() {
+		return mPokemonDefense;
+	}
+
+	public void setPokemonDefense(int pokemonDefense) {
+		mPokemonDefense = pokemonDefense;
+	}
+
+	public int getPokemonSpecialAttack() {
+		return mPokemonSpecialAttack;
+	}
+
+	public void setPokemonSpecialAttack(int pokemonSpecialAttack) {
+		mPokemonSpecialAttack = pokemonSpecialAttack;
+	}
+
+	public int getPokemonSpecialDefense() {
+		return mPokemonSpecialDefense;
+	}
+
+	public void setPokemonSpecialDefense(int pokemonSpecialDefense) {
+		mPokemonSpecialDefense = pokemonSpecialDefense;
+	}
+
+	public int getPokemonSpeed() {
+		return mPokemonSpeed;
+	}
+
+	public void setPokemonSpeed(int pokemonSpeed) {
+		mPokemonSpeed = pokemonSpeed;
 	};
+	
+	
 }
